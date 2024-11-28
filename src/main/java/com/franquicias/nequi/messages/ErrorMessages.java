508b5197e92aea122ff7a2c71a3c0c0cfe0dafce
ycuervob <64037012+ycuervob@users.noreply.github.com>
2024-11-28 16:32:53 -0500

    Added basic controller form Franquicia

---
+++ src/main/java/com/franquicias/nequi/messages/ErrorMessages.java
@@ -0,0 +1,17 @@
+package com.franquicias.nequi.messages;
+
+public class ErrorMessages {
+    public static final String FRANQUICIA_NOT_FOUND = "La franquicia no existe";
+    public static final String SUCURSAL_NOT_FOUND = "La sucursal no existe";
+    public static final String FRANQUICIA_ALREADY_EXISTS = "La franquicia ya existe";
+    public static final String SUCURSAL_ALREADY_EXISTS = "La sucursal ya existe";
+    public static final String FRANQUICIA_NAME_REQUIRED = "El nombre de la franquicia es requerido";
+    public static final String SUCURSAL_NAME_REQUIRED = "El nombre de la sucursal es requerido";
+    public static final String FRANQUICIA_ID_REQUIRED = "El id de la franquicia es requerido";
+    public static final String SUCURSAL_ID_REQUIRED = "El id de la sucursal es requerido";
+    public static final String FRANQUICIA_NAME_EMPTY = "El nombre de la franquicia no puede ser vacío";
+    public static final String FRANQUICIA_NAME_TOO_LONG = "El nombre de la franquicia es muy largo";
+    public static final String FRANQUICIA_NAME_TOO_SHORT = "El nombre de la franquicia es muy corto";
+    public static final String SUCURSAL_NAME_EMPTY = "El nombre de la sucursal no puede ser vacío";
+    public static final String FRANQUICIA_ID_EMPTY = "El id de la franquicia no puede ser vacío";
+}
