1d48a0490bc39749fa09e38cbc5ba527cf45dd7d
ycuervob <64037012+ycuervob@users.noreply.github.com>
2024-11-28 13:10:00 -0500

    Inicializacion del proyecto

---
+++ src/test/java/com/franquicias/nequi/NequiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franquicias.nequi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NequiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
